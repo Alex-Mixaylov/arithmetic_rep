8a6fba0350d601ecb0abdae5e28b491026f99712
Alex <guardamar81@gmail.com>
2024-05-01 17:44:19 +0400

    Update main.py
    Update main.py no division zero check

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 # main.py
-def add(x, y):
-    """Сложение x и y"""
-    return x + y
-def subtract(x, y):
-    """Вычитание y из x"""
-    return x - y
-def multiply(x, y):
-    """Умножение x на y"""
-    return x * y
-def divide(x, y):
-    """Деление x на y. Ошибка, если y равно 0."""
-    if y == 0:
-        return "Деление на ноль!"
-    return x / y
+import arithmetic as ar
+
+# Тестирование функций из модуля arithmetic
+a = float(input("Введите число a: "))
+b = float(input("Введите число b: "))
+
+print("Сложение:", ar.add(a, b))
+print("Вычитание:", ar.subtract(a, b))
+print("Умножение:", ar.multiply(a, b))
+print("Деление:", ar.divide(a, b))
+
+
